b8d2ff42ec412b51a9341849088d89d069f9e842
Rodrigo Corrales <rodrigocorrales@amigocloud.com>
2020-05-22 15:30:20 -0500

    updating the app with preview component and item component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepagre.component';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1> bolas </h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop/' component={ShopPage} />
       </Switch>
     </div>
   );
