7f798303f6dd09ad1e7389958122a592f5f3ab3d
Rodrigo Corrales <rodrigocorrales@amigocloud.com>
2020-05-21 20:34:54 -0500

    add base routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepagre.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1> bolas </h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
