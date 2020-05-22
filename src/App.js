b5405fcb146d3c32558e59d7edd2c6f092b23441
Rodrigo Corrales <rodrigocorrales@amigocloud.com>
2020-05-22 15:56:38 -0500

    add home header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Homepage from './pages/homepage/homepagre.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop/' component={ShopPage} />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop/' component={ShopPage} />
       </Switch>
     </div>
   );
