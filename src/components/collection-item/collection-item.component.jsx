b816ea2e4b15f769efded5f75accff6763a4b03c
Rodrigo Corrales <rodrigocorrales@amigocloud.com>
2020-10-17 17:57:41 -0500

    update elements

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styless.scss'
 
 const CollectionItem = ({ id, name, price, imageUrl}) => (
@@ -14,6 +15,7 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted> ADD to Cart</CustomButton>
 
   </div>
 );
